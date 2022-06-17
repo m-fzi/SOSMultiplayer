de893114d9ced8a845a3bb72ebad925af8114d92
m-fzi <mfzkgl@gmail.com>
2022-06-17 19:44:07 +0300

    Initial Commit

---
+++ SOSMultiplayer/SOSMultiplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOSMultiplayerApp.swift
+//  SOSMultiplayer
+//
+//  Created by f on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SOSMultiplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
